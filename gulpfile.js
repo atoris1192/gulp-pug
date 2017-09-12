d62cb4ebab9c2109d1800bba7aa0e9bcf126d470
k.niuishi <atoris1192@gmail.com>
2017-09-12 13:44:35 +0900

    gulp-pug first init

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+
+gulp.task('pug', () => {
+  return gulp.src(['./pug/**/*.pug', '!./pug/**/_*.pug'])
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(gulp.dest('./html/'));
+});
